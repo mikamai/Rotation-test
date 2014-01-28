74681af999b5a74bbac9a567c1bb5cb0e62ba4fb
Emanuel Carnevale <emanuel.carnevale@gmail.com>
2014-01-28 17:46:31 +0100

    Initial Commit

---
+++ Rotation test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Rotation test
+//
+//  Created by Emanuel Carnevale on 28/01/14.
+//  Copyright (c) 2014 Mikamai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Rotation testTests/Rotation_testTests.m
@@ -0,0 +1,34 @@
+//
+//  Rotation_testTests.m
+//  Rotation testTests
+//
+//  Created by Emanuel Carnevale on 28/01/14.
+//  Copyright (c) 2014 Mikamai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Rotation_testTests : XCTestCase
+
+@end
+
+@implementation Rotation_testTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
